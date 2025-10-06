afe6e4fe748c332e69ffca9599a5ffe382bddd26
mmarinoa <mmarinoa24@fpcoruna.afundacion.org>
2025-10-06 12:18:57 +0200

    Primera parte hecha, pantalla principal

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // script.js
+document.getElementById("scrollBtn").addEventListener("click", function() {
+  document.getElementById("secciones").scrollIntoView({ behavior: "smooth" });
+});
+
+function copiarTexto(id) {
+  const texto = document.getElementById(id).textContent;
+  navigator.clipboard.writeText(texto).then(() => {
+    alert(`Copiado: ${texto}`);
+  });
+}
+
+
 document.getElementById("mensajeBtn").addEventListener("click", function() {
   document.getElementById("saludo").textContent = "¡Hola Marta! Tu web está viva 🎉";
 });
